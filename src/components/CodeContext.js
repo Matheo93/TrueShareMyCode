8d21b0f00c39f7f227a0b982c92740ec00266b40
marveldc3 <162106935+marveldc3@users.noreply.github.com>
2024-07-09 10:59:35 +0200

    0

--- src/components/CodeContext.js
+++ src/components/CodeContext.js
@@ -1,5 +1,7 @@
 import { createContext, useState, useEffect } from 'react';
-import fetch from 'isomorphic-unfetch';
+import { KV } from '@vercel/kv';
+
+const kv = new KV();
 
 export const CodeContext = createContext();
 
@@ -9,16 +11,13 @@ export const CodeContextProvider = ({ children }) => {
   const addCode = async (newCode) => {
     const newCodes = [...codes, newCode];
     setCodes(newCodes);
-    const res = await saveCodes(newCodes);
-    if (!res.ok) {
-      throw new Error('Failed to save codes');
-    }
+    await kv.set('codes', JSON.stringify(newCodes));
   };
 
   useEffect(() => {
     async function fetchCodes() {
-      const data = await getCodes();
-      setCodes(data);
+      const data = await kv.get('codes');
+      setCodes(JSON.parse(data) || []);
     }
 
     fetchCodes();
@@ -30,25 +29,5 @@ export const CodeContextProvider = ({ children }) => {
     </CodeContext.Provider>
   );
 };
-
-async function saveCodes(codes) {
-  const res = await fetch('/api/codes', {
-    method: 'PUT',
-    body: JSON.stringify(codes),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-  return res;
-}
-
-async function getCodes() {
-  const res = await fetch('/api/codes');
-  if (!res.ok) {
-    throw new Error('Failed to fetch codes');
-  }
-  const data = await res.json();
-  return data;
-}
 
 export const useCodeContext = () => React.useContext(CodeContext);
