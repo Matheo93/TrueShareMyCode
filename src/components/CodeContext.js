dc4bd38ca225bff5fcc752548a91ce8fe3543c65
marveldc3 <162106935+marveldc3@users.noreply.github.com>
2024-07-09 10:49:21 +0200

    0

--- src/components/CodeContext.js
+++ src/components/CodeContext.js
@@ -1,22 +1,24 @@
-import { createContext, useState, useEffect } from'react';
-import { getStorage, setStorage } from '@vercel/storage';
+import { createContext, useState, useEffect } from 'react';
+import fetch from 'isomorphic-unfetch';
 
 export const CodeContext = createContext();
 
 export const CodeContextProvider = ({ children }) => {
   const [codes, setCodes] = useState([]);
 
-  const addCode = (newCode) => {
-    setCodes([...codes, newCode]);
-    setStorage('codes', codes);
+  const addCode = async (newCode) => {
+    const newCodes = [...codes, newCode];
+    setCodes(newCodes);
+    const res = await saveCodes(newCodes);
+    if (!res.ok) {
+      throw new Error('Failed to save codes');
+    }
   };
 
   useEffect(() => {
     async function fetchCodes() {
-      const storedCodes = await getStorage('codes');
-      if (storedCodes) {
-        setCodes(storedCodes);
-      }
+      const data = await getCodes();
+      setCodes(data);
     }
 
     fetchCodes();
@@ -28,5 +30,25 @@ export const CodeContextProvider = ({ children }) => {
     </CodeContext.Provider>
   );
 };
+
+async function saveCodes(codes) {
+  const res = await fetch('/api/codes', {
+    method: 'PUT',
+    body: JSON.stringify(codes),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  return res;
+}
+
+async function getCodes() {
+  const res = await fetch('/api/codes');
+  if (!res.ok) {
+    throw new Error('Failed to fetch codes');
+  }
+  const data = await res.json();
+  return data;
+}
 
 export const useCodeContext = () => React.useContext(CodeContext);
